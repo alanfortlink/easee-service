e0499bec1eb87aa3a5c0b37ec22eb043b988a63c
Alan M Silva <alanfortlink@gmail.com>
2020-06-14 21:20:39 +0100

    update

--- src/models/db.js
+++ src/models/db.js
@@ -1,109 +1,123 @@
+function getLatitude() {
+    const diff = Math.random() * 0.3;
+    return -23.533773 + diff;
+}
+
+function getLongitude() {
+    const diff = Math.random() * 0.3;
+    return -46.625290 + diff;
+}
+
+function getPrice() {
+    return parseInt((Math.random() * 5));
+}
+
 const _allPlaces = [{
         name: "KM 28 - Campeão - São Paulo - Norte",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "KM 34 - Caieiras - Norte",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Posto Campeão - KM 38 - Cajamar - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Praça de Pedágio de Campo Limpo - KM 39 - Caieiras - Norte",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Frango Assado - KM 44 - Cajamar - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "KM 56 - Jundiaí - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Graal 56 - KM 56 - Jundiaí - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Lago Azul - KM 58 - Jundiaí - Norte",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Lago Azul - KM 64 - Jundiaí - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Campeão - KM 68 - Jundiaí - Norte",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Posto Campeão - KM 68 - Jundiaí - Norte",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Jaal - KM 71 - Itupeva - Norte",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Serra Azul - KM 71 - Itupeva - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Campeão - KM 80 - Campinas - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "PGF 117 - KM 117 - Sumaré - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Graal 125 Norte - KM 125 - Santa Bárbara D'Oeste - Norte",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
     {
         name: "Graal 125 Sul - KM 125 - Santa Bárbara D'Oeste - Sul",
-        latitude: 0,
-        longitude: 0,
-        price: 0,
+        latitude: getLatitude(),
+        longitude: getLongitude(),
+        price: getPrice(),
     },
 ];
 
 function rand() {
-    return parseInt(Math.random() % 6);
+    return parseInt(Math.random() * 6);
 }
 
 exports.allPlaces = _allPlaces;
